248b2ea28951d0b81ddfd790242a2db5fb8d2e44
Vimal Jaiswal <jaiswalvimal1@gmail.com>
2022-11-11 21:11:35 +0530

    adding consoles to check

--- util/getImagesFromMongo.js
+++ util/getImagesFromMongo.js
@@ -14,7 +14,9 @@ export default async function getImagesFromMongo(albumId) {
       db = mongoose.connection;
       db.on("error", console.error.bind(console, "MongoDB connection error:"));
       try {
+        console.log("fetching from mongodb of albumID ", albumId)
         albums = await db.collection(albumId).find().toArray();
+        console.log(albums.length)
       } catch (err) {
         return albums;
       }
